f6e36f61e4223b99f8aa177454bc95b8d7fdcf2a
Paul Robson <paul@robsons.org.uk>
2019-06-06 23:17:05 +0100

    Outline Documentation

---
+++ scripts/basicblock.py
@@ -0,0 +1,162 @@
+# *******************************************************************************************
+# *******************************************************************************************
+#
+#		Name : 		basicblock.py
+#		Purpose :	Basic code block manipulator, with BASIC program addition.
+#		Date :		6th June 2019
+#		Author : 	Paul Robson (paul@robsons.org.uk)
+#
+# *******************************************************************************************
+# *******************************************************************************************
+
+import re,os,sys
+from gentokens import *
+from tokeniser import *
+
+# *******************************************************************************************
+#
+#										Basic Block Object
+#
+# *******************************************************************************************
+
+class BasicBlock(object):
+	def __init__(self,baseAddress = 0x0000,size = 0xFFFF,debug = False):
+		self.baseAddress = baseAddress											# Block information
+		self.blockSize = size
+		self.endAddress = baseAddress + size
+		self.data = [ 0 ] * size 												# containing data
+		for i in range(0,4):													# set 4 byte header
+			self.data[i] = ord(BasicBlock.ID[i])
+		self.debug = False
+		self.clearMemory()														# same as clear
+		self.memoryVariableCreated = False 										# allocated memory
+		self.debug = debug
+		self.tokeniser = Tokeniser()											# tokenises things
+		self.variables = {}														# variable info
+		self.lastProgramLineNumber = 0
+	#
+	#	Write binary out
+	#
+	def export(self,fileName):
+		h = open(fileName,"wb")													# write data as bytes
+		h.write(bytes(self.data))
+		h.close()
+	#
+	#	Erase all variables and code
+	#
+	def clearMemory(self):
+		self.writeWord(self.baseAddress+BasicBlock.HIGHPTR,self.endAddress)		# reset high memory
+		self.writeWord(self.baseAddress+BasicBlock.PROGRAM,0x0000)				# erase program
+		self.resetLowMemory()													# reset low memory
+	#
+	#	Rewrite the spacer and low memory
+	#
+	def resetLowMemory(self):
+		ptr = self.baseAddress+BasicBlock.PROGRAM 								# Where code starts
+		while self.readWord(ptr) != 0x0000:										# follow the code link chain
+			ptr = ptr + self.readWord(ptr)										# to the end.
+		self.writeWord(ptr+2,0xEEEE)											# write EEEE twice after it
+		self.writeWord(ptr+4,0xEEEE)											# visibility marker.
+		self.writeWord(self.baseAddress+BasicBlock.LOWPTR,ptr+6)				# free memory starts here.
+		return ptr 																# return where next line goes
+	#
+	#	Overwrite fast variable A-Z
+	#
+	def setFastVariable(self,variable,value):
+		variable = variable.upper()
+		assert re.match("^[\\@A-Z]$",variable) is not None						# check is fast variable
+		value = value & 0xFFFFFFFF												# make 32 bit uint
+		self.writeWord(self.baseAddress+(ord(variable)-ord('@'))*4+BasicBlock.FASTVARIABLES,value & 0xFFFF)
+		self.writeWord(self.baseAddress+(ord(variable)-ord('@'))*4+BasicBlock.FASTVARIABLES+2,value >> 16)
+	#
+	#	Allocate low memory (e.g. from program end up)
+	#
+	def allocateLowMemory(self,count):
+		addr = self.readWord(self.baseAddress+BasicBlock.LOWPTR)				# address to use
+		self.writeWord(self.baseAddress+BasicBlock.LOWPTR,addr+count)			# update offset
+		assert self.readWord(self.baseAddress+BasicBlock.LOWPTR) < self.readWord(self.baseAddress+BasicBlock.HIGHPTR)
+		return addr 
+	#
+	#	Allocate high memory (e.g. from top down)
+	#
+	def allocateHighMemory(self,count):
+		addr = self.readWord(self.baseAddress+BasicBlock.HIGHPTR) - count		# address to use
+		self.writeWord(self.baseAddress+BasicBlock.HIGHPTR,addr)				# update new high address
+		assert self.readWord(self.baseAddress+BasicBlock.LOWPTR) < self.readWord(self.baseAddress+BasicBlock.HIGHPTR)
+		return addr
+	#
+	#	Read a word from memory
+	#
+	def readWord(self,addr):
+		assert addr >= self.baseAddress and addr <= self.endAddress 			# validate
+		addr = addr - self.baseAddress 											# offset in data
+		return self.data[addr] + self.data[addr+1] * 256 						# return it
+	#
+	#	Write a word to memory
+	#
+	def writeWord(self,addr,data):
+		assert addr >= self.baseAddress and addr <= self.endAddress 			# validate it
+		data = data & 0xFFFF 													# force into 16 bit
+		self.data[addr-self.baseAddress] = data & 0xFF 							# store in structure
+		self.data[addr-self.baseAddress+1] = data >> 8
+		if self.debug:															# debug display
+			print("{0:04x} : {1:04x}".format(addr,data))
+	#
+	#	Read long as signed int
+	#
+	def readLong(self,addr):
+		val = self.readWord(addr)+(self.readWord(addr+2) << 16)					# read as 32 bit unsigned
+		if (val & 0x80000000) != 0:												# convert to signed
+			val = val - 0x100000000
+		return val
+	#
+	#		Add a line of BASIC
+	#
+	def addBASICLine(self,lineNumber,code):
+		assert not self.memoryVariableCreated									# check not created variables
+		if lineNumber is None or lineNumber == 0:								# default line number
+			lineNumber = self.lastProgramLineNumber + 1
+		assert lineNumber > self.lastProgramLineNumber and lineNumber <= 32767 	# check line number
+		pos = self.resetLowMemory()												# where does it go
+		self.lastProgramLineNumber = lineNumber 								# remember last
+		codeLine = self.tokeniser.tokenise(code) 								# convert to tokens
+		codeLine.append(0)														# EOL
+		codeLine.insert(0,lineNumber|0x8000) 									# insert line number
+		codeLine.insert(0,len(codeLine)*2+2)									# skip
+		codeLine.append(0)														# final program end marker
+		for t in codeLine:														# write it out
+			self.writeWord(pos,t)  
+			pos += 2
+		self.resetLowMemory() 													# and reset low memory
+	#
+	#		Export constants
+	#
+	def exportConstants(self,fileName):
+		self.handle = open(fileName.replace("/",os.sep),"w")
+		self._export("Block_FastVariables",BasicBlock.FASTVARIABLES)
+		self._export("Block_HashTable",BasicBlock.HASHTABLE)
+		self._export("Block_HashMask",BasicBlock.HASHMASK)
+		self._export("Block_LowMemoryPtr",BasicBlock.LOWPTR)
+		self._export("Block_HighMemoryPtr",BasicBlock.HIGHPTR)
+		self._export("Block_ProgramStart",BasicBlock.PROGRAM)
+		self._export("Block_HashTableEntrySize",BasicBlock.HASHMASKENTRYSIZE)
+		self.handle.close()
+	#
+	def _export(self,name,value):
+		self.handle.write("{0} = ${1:04x}\n".format(name,value))
+
+BasicBlock.ID = "BASC"															# ID
+BasicBlock.FASTVARIABLES = 0x10 												# Fast Variable Base
+BasicBlock.HASHTABLE = 0x80 													# Hash Table Base
+BasicBlock.LOWPTR = 0x10 														# Low Memory Allocation
+BasicBlock.HIGHPTR = 0x12 														# High Memory Allocation
+BasicBlock.PROGRAM = 0x100 														# First line of program
+BasicBlock.HASHMASK = 15 														# Hash mask (0,1,3,7,15)
+BasicBlock.HASHMASKENTRYSIZE = 16 												# Entries per table.
+
+if __name__ == "__main__":
+	blk = BasicBlock(0x4000,0x8000)
+	blk.addBASICLine(10,'42')
+	blk.export("temp/basic.bin")	
+	blk.exportConstants("temp/block.inc")
+
---
+++ scripts/tokeniser.py
@@ -0,0 +1,118 @@
+# *******************************************************************************************
+# *******************************************************************************************
+#
+#		Name : 		tokeniser.py
+#		Purpose :	Converts string into a list of tokens.
+#		Date :		6th June 2019
+#		Author : 	Paul Robson (paul@robsons.org.uk)
+#
+# *******************************************************************************************
+# *******************************************************************************************
+
+import re
+from gentokens import *
+
+# *******************************************************************************************
+#
+#									Tokeniser worker class
+#
+# *******************************************************************************************
+
+class Tokeniser(object):
+	def __init__(self):
+		if Tokeniser.tokens is None:
+			Tokeniser.tokens = [x for x in TokenList().getList() ]	# get tokens
+			Tokeniser.tokens.sort(key = lambda x:-len(x.name))		# sort by name, longest first
+	#
+	#		Tokenise a string into an array of words
+	#
+	def tokenise(self,s):
+		self.buffer = []
+		s = s.strip()
+		while s != "":
+			s = self.tokeniseOne(s).strip()
+		return self.buffer
+	#
+	#		Tokenise one item - a token, constant, identifier or quoted string.
+	#
+	def tokeniseOne(self,s):
+		t = self.findToken(s) 										# find a matching token
+		if t is not None:
+			self.buffer.append(t.id)								# add the token ID.
+			return s[len(t.name):]									# return post token.
+		#
+		m = re.match("^(\\d+)(.*)$",s)								# constant
+		if m is not None:
+			value = int(m.group(1))									# value of constant
+			assert value < 32768 * 4096,"Out of range "+s 			# too big.
+			if value >= 32768: 										# constant shift ?
+				self.buffer.append((value >> 15) | 0x1000)
+			self.buffer.append((value & 0x7FFF) | 0x8000)			# number token
+			return m.group(2)
+		#
+		m = re.match("^([a-z][a-z0-9]*)(\\$?)(\\(?)(.*)$",s.lower())# identifier with possible $ (
+		if m is not None:
+			ident = [self.convert(x) for x in m.group(1).upper()]	# shift into range
+			if len(ident) % 2 != 0:									# pad out to even length using zero.
+				ident.append(0) 									# 000000 is padding character.
+			for i in range(0,len(ident)-1,2):
+				word = 0x2000  + ident[i] + ident[i+1] * 45
+				if i != len(ident)-2: 								# add continuation marker.
+					word = word + 0x0800
+				if m.group(3) == "(":								# add type markers.
+					word = word + 0x1000
+				if m.group(2) == "$":
+					word = word + 0x2000
+				self.buffer.append(word)
+			return m.group(4)
+		#
+		m = re.match('^\\"(.*?)\\"(.*)$',s)							# quoted string.
+		if m is not None:
+			string = m.group(1)
+			assert len(string) < 252,"String too long "+s			
+			string = [ord(c) for c in string]						# byte string.
+			string.insert(0,len(string))							# add preceding length byte.
+			if len(string) % 2 != 0: 								# pad out to even length.
+				string.append(0)
+
+			self.buffer.append(0x0000+(len(string) >> 1)*2+2)		# header with object length.
+			for i in range(0,len(string),2):						# followed by the data.
+				self.buffer.append(string[i] + (string[i+1] << 8))
+			return m.group(2)
+		#
+		assert False,"Can't tokenise '{0}'".format(s)			
+	#
+	#		Convert character to tokenised format equivalent
+	#
+	def convert(self,c):
+		c = c.upper()
+		if c >= 'A' and c <= 'Z':
+			return ord(c)-ord('A')+1
+		if c >= '0' and c <= '9':
+			return int(c)+27
+		assert False
+	#
+	#		Find the token that s begins with. Relies on sort order of tokeniser.tokens
+	#
+	def findToken(self,s):
+		s = s.lower()												# all tokens are L/C
+		for e in Tokeniser.tokens:
+			if s.startswith(e.name):
+				return e
+		return None
+	#
+	#		Debugging helper
+	#
+	def tokeniseDebug(self,s):
+		print("**** "+s+" ****")
+		r = self.tokenise(s)
+		for b in r:
+			print("${0:04x}".format(b))
+
+Tokeniser.tokens = None
+
+if __name__ == "__main__":
+	tk = Tokeniser()
+	w = 'len(42) 32769 abcde zz ab ab$ ab( ab$( "abc" "" "abcd" '.split(" ")
+	for b in w:
+		tk.tokeniseDebug(b)
