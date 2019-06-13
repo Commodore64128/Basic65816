81e6d3666d53209d0b098fdb236d3076a8f910b1
Paul Robson <paul@robsons.org.uk>
2019-06-13 18:11:53 +0100

    Nested IF seems working

--- build/basic.bas
+++ build/basic.bas
@@ -1,4 +1,17 @@
-
-20 LET k=0 
-30 LET k=k+1
-50 IF k<10000 THEN GOTO 30
+20 	x = 0
+50 	repeat
+100	if (x & 1) = 0
+110		print x,"Even",
+112 	if x > 5
+118			print ">5"
+119 	else
+120 		print "<=5"
+129 	endif
+130 else
+134 	print x,"odd",
+135 	y = x
+136 	repeat:print y;" ";:y = y-1:until y = 0:print
+140	endif
+150 x = x + 1
+160 until x > 10
+180 stop	
--- source/commands/if.asm
+++ source/commands/if.asm
@@ -42,7 +42,7 @@ Function_IF: 	;; if
 		jsr 	EvaluateInteger 			; check success.
 		sty 	DTemp1 						; work out if non-zero
 		ora 	DTemp1 						
-		tax 								; success flag in X.
+		tay 								; success flag in Y.
 
 		lda 	(DCodePtr) 					; does THEN follow
 		cmp 	#thenTokenID
@@ -56,7 +56,7 @@ Function_IF: 	;; if
 		;
 		inc 	DCodePtr 					; skip over THEN token.
 		inc 	DCodePtr
-		cpx 	#0 							; was it successful.
+		cpy 	#0 							; was it successful.
 		beq 	_FIFEndOfLine 				; if not, go to the end of the line.
 		;
 		lda 	(DCodePtr) 					; look to see if there is a number there.
@@ -94,5 +94,140 @@ _FIFContinue:
 		; ************************************************************************************
 		;
 _FIFExtended:
-		bra 	_FIFExtended
+		inc 	DStack 						; put IF on the top of the stack
+		inc 	DStack
+		lda 	#ifTokenID 
+		ldx 	DStack
+		sta 	$00,x
+		;
+		tya 								; see if the test was passed.
+		beq 	_FIXSkip 					; if zero then it has failed.
+		rts 								; test passed, so continue executing
+		;
+		;		Test Failed.
+		;
+_FIXSkip:
+		lda 	#elseTokenID 				; scan forward till found either ELSE or ENDIF
+		ldx 	#endifTokenID 				; at the same level.
+		jsr 	ScanForwardLevel 			; scan forward, returns what found in A.
+		inc 	DCodePtr 					; skip over the ELSE or ENDIF
+		inc 	DCodePtr
+		cmp 	#endifTokenID 				; if ENDIF token ID, then throw the TOS as ended
+		bne 	_FIXNoThrow
+		dec 	DStack 						; throw the token IF on the stack top.
+		dec 	DStack
+_FIXNoThrow:
+		rts
+
+		;
+		;		ELSE. If you execute ELSE, then skip forward to the ENDIF on this level.
+		;
+Handler_ELSE:	;; else 
+		ldx 	DStack	 					; check the top of stack is IF.
+		lda 	$00,x
+		cmp 	#ifTokenID
+		bne 	_HEBadStructure
+		lda 	#endifTokenID 				; only searching one token.
+		ldx 	#$0000			
+		jsr 	ScanForwardLevel 			; so this will find the ENDIF
+		;
+		inc 	DCodePtr 					; skip over the ENDIF
+		inc 	DCodePtr
+		dec 	DStack 						; throw the token IF on the stack top.
+		dec 	DStack
+		rts
+
+_HEBadStructure:	
+		#error 	"Else without If"
+
+		;
+		;		ENDIF. If you execute ENDIF, then just test and throw TOS.
+		;
+Handler_ENDIF:	;; endif
+		ldx 	DStack	 					; check the top of stack is IF.
+		lda 	$00,x
+		cmp 	#ifTokenID
+		bne 	_HEIBadStructure
+		;
+		dec 	DStack 						; throw the token IF on the stack top.
+		dec 	DStack
+		rts
+
+_HEIBadStructure:	
+		#error 	"Else without If"
+
+;
+;		Forward scanner. Work forward through the current position looking for either the
+;		token in A or X (use 0 if you only want one). This allows for up and down counts
+;		caused by keyword+ and keyword-
+;
+
+ScanForwardLevel:
+		sta 	DTemp1 						; save test in DTemp1 and DTemp1+2
+		stx 	DTemp1+2 	
+		lda 	DLineNumber 				; save original line number for error
+		sta 	DTemp2				
+		ldx 	#0 							; X is the level counter.
+_SFLLoop:
+		cpx 	#0 							; if X != 0 then don't test tokens for exit.
+		bne 	_SFLNoCheck
+		;
+		lda 	(DCodePtr)					; what's there
+		beq 	_SFLNoCheck 				; don't check zero
+		cmp 	DTemp1 						; does it match either token ?
+		beq 	_SFLFound
+		cmp 	DTemp1+2
+		bne 	_SFLNoCheck
+_SFLFound:
+		rts
+		;
+_SFLNoCheck:
+		lda 	(DCodePtr) 					; what is the token.
+		beq 	_SFLNextLine 				; if $0000 go to the next line (end of line marker)				
+		cmp 	#$0100 						; is it a string.
+		bcc 	_SFLSkipString 				; then handle that.
+		;
+		;		Not a string or end of line marker
+		;
+		inc 	DCodePtr 					; skip over the token
+		inc 	DCodePtr
+		tay 								; put token in Y temporarily.
+		and 	#$E000 						; if not a keyword 010x xxxx xxxx xxxx
+		cmp 	#$2000
+		bne 	_SFLLoop
+		tya 								; get the token back.
+		and 	#15 << 9 					; get out token type xxxA AAAx xxxx xxxx
+		cmp 	#15 << 9 					; 15 is a standard keyword
+		beq 	_SFLLoop
+		cmp 	#13 << 9 					; < 13 is also standard
+		bcc 	_SFLLoop
+		inx 								; increment the level.
+		cmp 	#14 << 9 					; if keyword +, loop back.
+		beq 	_SFLLoop
+		dex 								; decrement the level
+		dex 								; one overdoes the previous inx.
+		bpl 	_SFLLoop 					; if gone -ve then error.
+_SFLError:		
+		lda 	DTemp2 						; get original line number
+		sta 	DLineNumber
+		#error	"Structure imbalance"
+		;
+_SFLSkipString:
+		and 	#$00FF 						; token length of string
+		clc
+		adc 	DCodePtr 					; add to code pointer and save out
+		sta 	DCodePtr
+		bra 	_SFLLoop 					; go round again.
+		;
+_SFLNextLine:
+		ldy 	DCodePtr 					; put code pointer into Y
+		lda 	$0002,y 					; look at the link for the next line.
+		beq 	_SFLError 					; if zero, then there's no more code to search.
+		lda 	$0004,y 					; update the line number so it's still correct.
+		sta 	DLineNumber
+		clc
+		lda 	DCodePtr
+		adc 	#6 							; skip $00 marker, link, new line
+		sta 	DCodePtr
+		bra 	_SFLLoop 					; and start this one.
 
