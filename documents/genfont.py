f4953cde86d370da77eb96dee8b4b39012f67564
Paul Robson <paul@robsons.org.uk>
2019-06-23 22:47:19 +0100

    Changed demo font for thinner

--- documents/genfont.py
+++ documents/genfont.py
@@ -1,5 +1,5 @@
-font = [x for x in bytes(open("cpc6128.rom","rb").read(-1))]
-font = font[0x3800:0x4000]
+font = [x for x in bytes(open("challenger.rom","rb").read(-1))]
+#font = font[0x3800:0x4000]
 h = open("c64font.h","w")
 font = ",".join([str(x) for x in font])
 h.write("const BYTE8 font[] = { "+font+ "};\n\n")
